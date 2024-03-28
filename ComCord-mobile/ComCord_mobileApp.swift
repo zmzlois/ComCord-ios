16a799531a04709bff2cb3d6b6a037af956b581d
zmzlois <lois.z@comcord.vision>
2024-03-28 13:45:40 +0000

    Initial Commit

---
+++ ComCord-mobile/ComCord_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComCord_mobileApp.swift
+//  ComCord-mobile
+//
+//  Created by Zhaomian Zhao on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComCord_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
