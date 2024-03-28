6a908ecff46ef81c41fad44d4d6a066421ceb96a
zmzlois <lois.z@comcord.vision>
2024-03-28 19:24:16 +0000

    navigating between views

---
+++ ComCord-mobile/Views/DetailEditView.swift
@@ -0,0 +1,59 @@
+//
+//  DetailEditView.swift
+//  ComCord-mobile
+//
+//  Created by Zhaomian Zhao on 28/03/2024.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...120, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .font(.caption)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
